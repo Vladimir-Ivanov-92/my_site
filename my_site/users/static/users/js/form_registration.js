9a0fa5113aa88bc228100c48e632efb192e63462
vova2092 <vovai2092@yandex.ru>
2023-02-07 16:49:33 +0300

    Update: added in form_registration.js information, about email verification + change in email_is_verificate.html

--- my_site/users/static/users/js/form_registration.js
+++ my_site/users/static/users/js/form_registration.js
@@ -1,18 +1,28 @@
 $(function ($) {
     $('#form_registration_ajax').submit(function (e) {
         e.preventDefault()
-        console.log(this)
         $.ajax({
             type: this.method,
             url: this.action,
             data: $(this).serialize(),
             dataType: 'json',
             success: function (response) {
-                console.log('ok - ', response)
-                window.location.reload()
+                $('.alert-danger').addClass('d-none')
+                $('.modal-footer').addClass('d-none')
+                $('.modal-header').html(
+                    '<h1 class="modal-title fs-5" id="exampleModalLabel">Успешная регистрация!</h1>\n' +
+                    ' <button type="button" class="btn-close" data-bs-dismiss="modal"\n' +
+                    '           aria-label="Close"></button>')
+                $('.modal-body').html(
+                    '<div class="text-center"><text>' +
+                    'На указанную вами электронную почту направлено письмо, ' +
+                    'для подтверждения электронного адреса.</text></div>');
+                $(document).click(function (e) {
+                    e.preventDefault()
+                    window.location.reload()
+                })
             },
-            error: function(response) {
-                console.log('err - ', response)
+            error: function (response) {
                 if (response.status === 400) {
                     $('.alert-danger').text(response.responseJSON.error).removeClass('d-none')
                 }
